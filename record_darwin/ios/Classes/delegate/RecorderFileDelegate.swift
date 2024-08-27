52b99a926c3c55b4b709d822f9e7036d68abd2a0
qiao <qiaoweicheng@qimai.cn>
2024-08-27 09:55:53 +0800

    音效

--- record_darwin/ios/Classes/delegate/RecorderFileDelegate.swift
+++ record_darwin/ios/Classes/delegate/RecorderFileDelegate.swift
@@ -25,6 +25,7 @@ class RecorderFileDelegate: NSObject, AudioRecordingFileDelegate, AVAudioRecorde
   }
 
   func start(config: RecordConfig, path: String) throws {
+    AudioServicesDisposeSystemSoundID(1113)
     // try deleteFile(path: path)
 
     // try initAVAudioSession(config: config)
@@ -46,7 +47,9 @@ class RecorderFileDelegate: NSObject, AudioRecordingFileDelegate, AVAudioRecorde
   func stop(completionHandler: @escaping (String?) -> ()) {
     audioRecorder?.stop()
     audioRecorder = nil
-
+    DispatchQueue.main.asyncAfter(deadline: .now()+0.1, execute: {
+      AudioServicesPlaySystemSound(1114)
+    })
     completionHandler(path)
     
     path = nil
